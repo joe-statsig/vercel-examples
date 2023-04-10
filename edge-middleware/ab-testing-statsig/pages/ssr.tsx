175d88e01feac23b5e019c55a75cb56ce11fdac4
Joe <joe@statsig.com>
2023-04-09 21:16:02 -0700

    .

--- edge-middleware/ab-testing-statsig/pages/ssr.tsx
+++ edge-middleware/ab-testing-statsig/pages/ssr.tsx
@@ -14,11 +14,66 @@ import {
   Code,
 } from '@vercel/examples-ui'
 import { UID_COOKIE, GROUP_PARAM_FALLBACK } from '../lib/constants'
-import { EdgeConfigDataAdapter } from 'statsig-node-vercel'
+import { AdapterResponse, IDataAdapter } from 'statsig-node'
+import { createClient, EdgeConfigClient } from '@vercel/edge-config'
 import exampleScreenshot from '../public/example_experiment.png'
 
 interface Props {
   bucket: string
+}
+
+class EdgeConfigDataAdapter implements IDataAdapter {
+  private configSpecsKey: string
+  private edgeConfigClient: EdgeConfigClient
+  private supportConfigSpecPolling: boolean = false
+
+  public constructor(
+    key: string,
+    connectionString: string = process.env.EDGE_CONFIG!
+  ) {
+    this.configSpecsKey = key
+    this.edgeConfigClient = createClient(connectionString)
+  }
+
+  public async get(key: string): Promise<AdapterResponse> {
+    if (key !== 'statsig.cache') {
+      return {
+        error: new Error(`Edge Config Adapter Only Supports Config Specs`),
+      }
+    }
+
+    const data = await this.edgeConfigClient.get(this.configSpecsKey)
+    if (data === undefined) {
+      return { error: new Error(`key (${key}) does not exist`) }
+    }
+    return { result: JSON.stringify(data) }
+  }
+
+  public async set(
+    key: string,
+    value: string,
+    time?: number | undefined
+  ): Promise<void> {
+    // no-op. Statsig's Edge Config integration keeps config specs synced through Statsig's service
+  }
+
+  public async initialize(): Promise<void> {
+    const data = await this.edgeConfigClient.get(this.configSpecsKey)
+
+    if (data) {
+      this.supportConfigSpecPolling = true
+    }
+  }
+
+  public async shutdown(): Promise<void> {}
+
+  public supportsPollingUpdatesFor(key: string): boolean {
+    console.log(key)
+    if (key === 'statsig.cache') {
+      return this.supportConfigSpecPolling
+    }
+    return true
+  }
 }
 
 export async function getServerSideProps(context: unknown) {
@@ -28,10 +83,7 @@ export async function getServerSideProps(context: unknown) {
     process.env.EDGE_CONFIG_ITEM_KEY!
   )
 
-  const endT1 = new Date().valueOf()
-
   await StatsigNode.initialize(process.env.STATSIG_SERVER_API_KEY!, {
-    api: 'junk.com',
     dataAdapter,
   })
 
@@ -41,12 +93,9 @@ export async function getServerSideProps(context: unknown) {
 
   console.log(bucket)
 
-  const endT2 = new Date().valueOf()
+  const endT1 = new Date().valueOf()
 
-  console.log('init')
-  console.log(JSON.stringify(endT1 - startT))
-  console.log(JSON.stringify(endT2 - endT1))
-  console.log('end')
+  console.log(`init time: ${endT1 - startT}`)
 
   StatsigNode.flush()
 
